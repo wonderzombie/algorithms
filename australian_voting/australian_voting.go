36d3fab39f161bf88b416c0e502763d0074ff8bb
Matthew <wonderzombie@gmail.com>
2011-07-10 22:09:59 -0700

    Rough first attempt.
    Some basic logic for tallying (proof of concept?), and a simple test for it.
    
    Still no logic for eliminating candidates with the least votes or anything.

--- australian_voting/australian_voting.go
+++ australian_voting/australian_voting.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+//	"fmt"
+)
+
+type Ballot []int
+
+func tallyVotes(candidates []string, ballots []Ballot, round int) map[string] int {
+	tally := make(map[string] int)
+	for _, candidate := range candidates {
+		tally[candidate] = 0
+	}
+
+	for _, b := range ballots {
+		i := b[round - 1]
+		cand := candidates[i - 1]
+		tally[cand] += 1
+	}
+
+	return tally
+}
+
 func main() {
 
 }
-
--- australian_voting/australian_voting_test.go
+++ australian_voting/australian_voting_test.go
@@ -1,5 +1,22 @@
 package main
 
 import (
+//	"fmt"
 	"testing"
 )
+
+func TestTallyVotes(t *testing.T) {
+	candidates := []string { "Alice", "Bob", "Chuck" }
+	ballots := []Ballot {
+		Ballot { 1, 2, 3 },
+		Ballot { 2, 3, 1 },
+		Ballot { 1, 2, 3 },
+		Ballot { 1, 3, 2 },
+	}
+
+	tally := tallyVotes(candidates, ballots, 1)
+
+	if tally["Alice"] != 3 || tally["Bob"] != 1 || tally["Chuck"] != 0 {
+		t.Fatalf("Expected Alice: 3, Bob: 1, and Chuck: 0. Actual: %v", tally)
+	}
+}
