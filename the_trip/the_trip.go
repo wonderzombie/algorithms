2c715ab2d4399545a577886f1c9fbec03a2cfefd
Matthew <mjm99f@gmail.com>
2011-07-10 17:00:02 -0700

    Read expense report from user.
    Read a single expense report from the user.

--- the_trip/the_trip.go
+++ the_trip/the_trip.go
@@ -4,6 +4,34 @@ import (
 	"fmt"
 )
 
+type ExpenseReport struct {
+	expenses []float32
+}
+
+func getInput(a ...interface{}) {
+	_, err := fmt.Scanln(a...)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readExpenses() *ExpenseReport {
+	var students int
+	// count, err := fmt.Scanln(&students)
+	getInput(&students)
+
+	report := new(ExpenseReport)
+	for i := 0; i < students; i++ {
+		var expense float32
+		getInput(&expense)
+		report.expenses = append(report.expenses, expense)
+	}
+
+	return report
+}
+
 func main() {
-	fmt.Println("asdf")
+	report := readExpenses()
+	fmt.Printf("Expense report: %v\n", report)
 }
