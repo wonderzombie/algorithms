dddca2f9a8366b93a87d8a5c49b291dba5f190f4
Matthew <wonderzombie@gmail.com>
2011-08-04 22:58:56 -0700

    First pass at jolly_jumper. WIP.

---
+++ jolly_jumper/jolly_jumper.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "math"
+)
+
+func absDifference(a float64, b float64) float64 {
+  diff := a - b
+  abs := math.Fabs(diff)
+  return abs
+}
+
+func isJolly(seq []float64) bool {
+  seqLen := len(seq)
+
+  return true
+}
+
+func main() {
+
+}
